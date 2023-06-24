bf6918329c267035d41e0e2a7c8bc9d1b0e7a053
nickingenito <ningenito@gmail.com>
2023-06-24 17:21:51 -0500

    Create routes from async fetch API call

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 const API_KEY = config.API_KEY;
 
-class BusRider {
-    constructor(startLocation, endLocation) {
-        this.startLocation = startLocation;
-        this.endLocation = endLocation;
-    }
-    useCurrent(){
-        const currentLocation = getCurrent();
-        startLocation = currentLocation;
-    }
-    useCustom(){
-        let customLocation = document.getElementById("origin").value;
-        startLocation = customLocation;
+async function populate() {
+    const requestURL = "./routes.json";
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const routes = await response.json();
+
+    createRoutes(routes);
+}
+
+function createRoutes(routes){
+    const container = document.getElementById('route-list');
+
+    for (const route of routes){
+        const newRoute = document.createElement('button');
+        const newHeader = document.createElement('h3');
+
+        newHeader.textContent = route.name
+        newRoute.classList.add("route-card");
+        if(!route.active){
+            newRoute.classList.add("inactive");
+        }
+
+        newRoute.appendChild(newHeader);
+        container.appendChild(newRoute);
     }
 }
 
-class BusRoute {
-    constructor(fare, driver, activity, time, numBuses, routeID){
-        this.fare = fare;
-        this.driver = driver;
-        this.activity = activity;
-        this.time = time;
-        this.numBuses = numBuses;
-        this.routeID = routeID;
-    }
-}
-
-class BusStop {
-    constructor(address, active){
-        this.address = address;
-        this.active = active;
-    }
-}
-
-const MG114 = "https://www.google.com/maps/embed/v1/directions?key=" + API_KEY + "&origin=33.211025,-97.146251&waypoints=33.208244,-97.147459|33.208278,-97.150049|33.206803,-97.152148|33.209621,-97.155453|33.211532,-97.153609|33.213704,-97.151497|33.213977,-97.148428&destination=33.211025,-97.146251"
-const NT124 = "https://www.google.com/maps/embed/v1/directions?key=" + API_KEY + "&origin=33.213315,-97.173426&waypoints=33.215423,-97.166131|33.218690,-97.147173|33.211543,-97.144716&destination=33.213315,-97.173426"
-
-
+populate();
 
 function showRoute(routeSRC){
     document.getElementById('map').src = routeSRC;
