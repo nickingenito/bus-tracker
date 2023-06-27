7dddd99a22a61fe48bc523cbad88de29c27e6bbd
ddb0240 <135185056+ddb0240@users.noreply.github.com>
2023-06-27 06:14:47 -0500

    Linked List Functions
    Forgot to add functions for the linked list class

--- script.js
+++ script.js
@@ -45,6 +45,23 @@ class LinkedList {
         this.head = null;
         this.tail = null;
     }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        return this;
+    }
+    cycle() {
+        const temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.tail = temp;
+    }
 }
 
 const MG114 = "https://www.google.com/maps/embed/v1/directions?key=" + API_KEY + "&origin=33.211025,-97.146251&waypoints=33.208244,-97.147459|33.208278,-97.150049|33.206803,-97.152148|33.209621,-97.155453|33.211532,-97.153609|33.213704,-97.151497|33.213977,-97.148428&destination=33.211025,-97.146251"
