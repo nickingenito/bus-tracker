3ddd5c56bfce6f8ad4f271c2855e56c2992ee403
nickingenito <ningenito@gmail.com>
2023-06-27 14:14:12 -0500

    Add linked list functions

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        return this;
+    }
+    cycle() {
+        const temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.tail = temp;
+    }
+}
+
 function toggleInactive(){
     const inactives = document.querySelectorAll(".inactive")
     inactives.forEach((obj) => {
